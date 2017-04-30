8c6ab151e9ae3a148b706544ccf1f9af4a223816
joeyarbrough <jcyarbro88@gmail.com>
2017-04-30 12:53:25 -0500

    home screen is set up

---
+++ src/controllers/HomeScreenController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class HomeScreenController {
+
+
+    public Button newOrderButton;
+    public Button pickupButton;
+    public Button deliveryButton;
+    public Button openOrdersButton;
+    public Button reviseOrderButton;
+    public Button openRegisterButton;
+    public TextField loggedInTextField;
+    public TextField idTextField;
+
+
+}
